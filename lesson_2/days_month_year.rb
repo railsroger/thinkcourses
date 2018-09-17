2d7f6a13ec034fc55402f4ac45e60708c2b075ed
railsroger <railsroger@gmail.com>
2018-09-17 19:04:20 +0300

    Fix Homework

--- lesson_2/days_month_year.rb
+++ lesson_2/days_month_year.rb
@@ -9,6 +9,9 @@ year = gets.chomp.to_i
 
 month_all = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-month_all.each_with_index { |m, i| day += m if month - 1 > i and day += 1 if year % 400 == 0 || (year % 4 == 0 && year % 100 != 0) }
+month_all.each_with_index { |m, i| day += m if month - 1 > i } 
+  if year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
+    day += 1
+  end
 
 puts "Порядковый номер даты: #{day}"
